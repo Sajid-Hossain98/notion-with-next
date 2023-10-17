a9231a236e4a5883d6033d760340def5878d154a
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-17 15:58:34 +0600

    Theme(Light and dark mode) implementation

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 
@@ -16,7 +17,7 @@ export const Navbar = () => {
       <Logo />
 
       <div className="w-full md:ml-auto flex items-center justify-between md:justify-end gap-x-2">
-        Login
+        <ModeToggle />
       </div>
     </div>
   );
