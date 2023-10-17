ee5e8a834c14e002296b8f1a9f6287ebb7851a18
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-17 13:15:56 +0600

    Footer component

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+
+export const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+      <div className="md:ml-auto w-full flex items-center justify-between md:justify-end gap-x-2 text-muted-foreground">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms and Condition
+        </Button>
+      </div>
+    </div>
+  );
+};
