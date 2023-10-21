5d3b0c21ea436d696b44c0e46fd2d17075a262f3
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-21 12:27:39 +0600

    trashbox with search and restoring functionalities

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,6 +24,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -170,7 +171,7 @@ export const Navigation = () => {
               side={isMobile ? "bottom" : "right"}
               className="p-0 w-72"
             >
-              <p>Trash box</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
