68826f6b22b8c1a1db4b05fc385c157302fcea74
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-28 15:26:45 +0600

    Dynamic import of the Editor

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Editor } from "@/components/Editor";
 import { Cover } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 interface DocumentIdPageProps {
   params: {
@@ -15,6 +16,11 @@ interface DocumentIdPageProps {
 }
 
 const DocumentsIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/Editor"), { ssr: false }),
+    []
+  );
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
