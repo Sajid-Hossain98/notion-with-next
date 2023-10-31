dd09cec2b389174bf6aeabe997d473b8e15090a0
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-31 22:10:21 +0600

    metadata: preview page

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sotion | Preview",
+  description:
+    "This is just a preview of any published documents/notes created on sotion by a user.",
+};
+
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return <div className="h-full dark:bg-[#1F1F1F]">{children}</div>;
 };
